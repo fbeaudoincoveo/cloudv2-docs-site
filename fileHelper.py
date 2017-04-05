52a5fda742fffed1292f0488ea9572f53a9b6a43
Frederic Beaudoin <fbeaudoin@coveo.com>
2017-04-05 13:33:18 -0400

    Upstream_update_2017-04-05_13:33:18

--- fileHelper.py
+++ fileHelper.py
@@ -11,5 +11,5 @@ def create_file_from_data(data, file_to_create, debug=False):
 
 def create_yaml_file_from_json_file(input_json_file, output_yaml_file, debug=False):
     if debug:
-        print "Creating YAML file %s from JSON file %s" % output_yaml_file % input_json_file
-    call("json2yaml %s > %s" % input_json_file % output_yaml_file, shell=True)
+        print "Creating YAML file %s from JSON file %s" % (output_yaml_file, input_json_file)
+    call("json2yaml %s > %s" % (input_json_file, output_yaml_file), shell=True)
