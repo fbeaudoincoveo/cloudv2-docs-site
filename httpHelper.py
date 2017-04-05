cbbc9c2ec0f21aae66da0f645fad5528a1119926
Frederic Beaudoin <fbeaudoin@coveo.com>
2017-04-05 13:23:28 -0400

    Upstream_update_2017-04-05_13:23:28

--- httpHelper.py
+++ httpHelper.py
@@ -2,9 +2,13 @@ from requests import get
 from requests import post
 
 
-def basic_get(uri):
+def basic_get(uri, debug=False):
+    if debug:
+        print "Sending basic HTTP GET request to %s" % uri
     return get(uri)
 
 
-def basic_post(uri, headers=None, body=None):
+def basic_post(uri, headers=None, body=None, debug=False):
+    if debug:
+        print "Sending basic HTTP POST request to %s" % uri
     return post(uri, headers, body)
