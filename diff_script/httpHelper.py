a7c0317e1febbc09889529958cf33bbf5c947b2f
Frederic Beaudoin <fbeaudoin@coveo.com>
2017-04-10 14:53:13 -0400

    Upstream_update_2017-04-10_14:53:13

--- diff_script/httpHelper.py
+++ diff_script/httpHelper.py
@@ -1,5 +1,6 @@
 from requests import get
 from requests import post
+from requests import put
 from json import dumps
 
 
@@ -13,3 +14,9 @@ def basic_post(uri, headers=None, body=None, debug=False):
     if debug:
         print "Sending basic HTTP POST request to %s" % uri
     return post(uri, headers=headers, data=dumps(body))
+
+
+def basic_put(uri, headers=None, body=None, debug=False):
+    if debug:
+        print "Sending basic HTTP PUT request to %s" % uri
+    return put(uri, headers=headers, data=dumps(body))
---
+++ pages/api_explorer/SearchApi.md
@@ -0,0 +1,8 @@
+---
+layout: redoc_page
+title: 'SearchApi'
+categories: api_docs
+swagger: ../api_docs/SearchApi.yml
+permalink: ../pages/api_explorer/SearchApi
+ghPagesSiteName: /cloudv2-docs-site
+---
