44a227a0b30d4b896c1029346eb30133adef4194
François Dallaire <fdallaire@coveo.com>
2017-10-17 16:41:22 -0400

    AWS Lambda URL rewrite script
    Rewrite to ignore any path after the id (not tested yet).

---
+++ scripts/CloudFrontUrlRewrite.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/* Lambda sample originally from:
+ *  - https://linuxacademy.com/howtoguides/posts/show/topic/19955-url-rewriting-in-aws-cloudfront
+ *  - http://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/lambda-examples.html#lambda-examples-redirect-based-on-country
+ */
+
+/* This is an origin request function */
+exports.handler = (event, context, callback) => {
+    // The request we want to re-map
+    const request = event.Records[0].cf.request;
+    // Get the original 'uri' property to overwrite
+    const pathSearchHash = request.uri;
+
+    // Keep only the ID initial part together with the search query parameters and hash
+    // Example: /4/api-explorer/Coveo-Cloud-V2-APIs?extUrl=1&test=123#somehash  = > /4/?extUrl=1&test=123#somehash
+    request.uri = pathSearchHash.replace(/^(\/[0-9]*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/,"$1$3");
+
+    // quit the lambda and let the request chain continue
+    callback( null, request );
+};
