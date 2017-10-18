f3dc57da40fd68086a22f413e9bebdfd86715489
François Dallaire <fdallaire@coveo.com>
2017-10-18 17:27:11 -0400

    Making the dynamic URL completion conditional on staging too
    The URL rewrite AWS Lambda is now active. Prevent completion on
    localhost to avoid reloading and braking the URL.

--- assets/coveo-community-custom.js
+++ assets/coveo-community-custom.js
@@ -18,16 +18,23 @@ $(window).ready(function() {
             $('#pagesMenu > a').addClass('currentSection');
     }
 
-    // Dynamically completing the ID based URL with the page path and title if extUrl=1
-    var originalPath = window.document.location.pathname;
-    var originalHash = window.document.location.hash;
-    var originalSearch = window.document.location.search;
+    // Dynamically completing the ID based URL with the page path and title
+    var location = window.document.location;
+    var host = location.host;
+    var originalSearch = location.search;
     var isExtUrl = originalSearch.includes("extUrl=1");
-    var title = $('head > title').text();
-    var codedTitle = title.replace(/([~!@#$%^&*()_+=`{}\[\]\|\\:;'<>,.\/? ])+/g, '-').replace(/^(-)+|(-)+$/g,'');
-    var sourcePath = $('meta[name=sourcePath]').attr("content");
-    var collectionLabel = sourcePath.substr(0, sourcePath.indexOf('/')).replace(/^_/, '');
-    if (isExtUrl) {
-        window.history.pushState("", "", originalPath + collectionLabel + '/' + codedTitle + originalHash + originalSearch);
+    // Complete URL only when on staging or with the ?extUrl=1 query parameter
+    if (host == "d3in5cp0cx0iu6.cloudfront.net" || isExtUrl ) {
+        var originalPath = location.pathname;
+        // Remove any extended URI from originalPath
+        var nativePath = originalPath.replace(/^(\/[0-9]*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/,"$1$3");
+        var originalHash = location.hash;
+
+        var title = $('head > title').text();
+        var codedTitle = title.replace(/([~!@#$%^&*()_+=`{}\[\]\|\\:;'<>,.\/? ])+/g, '-').replace(/^(-)+|(-)+$/g,'');
+        var sourcePath = $('meta[name=sourcePath]').attr("content");
+        var collectionLabel = sourcePath.substr(0, sourcePath.indexOf('/')).replace(/^_/, '');
+        window.history.pushState("", "", nativePath + collectionLabel + '/' + codedTitle + originalHash + originalSearch);
     }
+
 });
