d91f830d4bc5f70203dd880c51f16f8d6d7bdf98
François Dallaire <fdallaire@coveo.com>
2017-10-18 17:28:47 -0400

    Matching the Lambda code in prod for d3in5cp0cx0iu6.cloudfront.net

--- scripts/CloudFrontUrlRewrite.js
+++ scripts/CloudFrontUrlRewrite.js
@@ -3,6 +3,7 @@
 /* Lambda sample originally from:
  *  - https://linuxacademy.com/howtoguides/posts/show/topic/19955-url-rewriting-in-aws-cloudfront
  *  - http://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/lambda-examples.html#lambda-examples-redirect-based-on-country
+ *
  */
 
 /* This is an origin request function */
@@ -10,11 +11,11 @@ exports.handler = (event, context, callback) => {
     // The request we want to re-map
     const request = event.Records[0].cf.request;
     // Get the original 'uri' property to overwrite
-    const pathSearchHash = request.uri;
+    const originalUri = request.uri;
 
     // Keep only the ID initial part together with the search query parameters and hash
     // Example: /4/api-explorer/Coveo-Cloud-V2-APIs?extUrl=1&test=123#somehash  = > /4/?extUrl=1&test=123#somehash
-    request.uri = pathSearchHash.replace(/^(\/[0-9]*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/,"$1$3");
+    request.uri = originalUri.replace(/^(\/[0-9]*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/,"$1$3");
 
     // quit the lambda and let the request chain continue
     callback( null, request );
