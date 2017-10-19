45277cc3bc9a5bd37bdc5d8fe53d017c84d06ccb
François Dallaire <fdallaire@coveo.com>
2017-10-19 13:34:05 -0400

    Moving the AWS Lambda rewrite URL script

--- scripts/AWS/CloudFrontUrlRewrite.js
+++ scripts/AWS/CloudFrontUrlRewrite.js
@@ -1,9 +1,12 @@
 'use strict';
 
-/* Lambda sample originally from:
+/* Lambda for Jekyll-powered documentation site to rewrite native ID-based URL to remove
+ * dynamically added path.
+ *
+ * Lambda sample originally from:
  *  - https://linuxacademy.com/howtoguides/posts/show/topic/19955-url-rewriting-in-aws-cloudfront
  *  - http://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/lambda-examples.html#lambda-examples-redirect-based-on-country
- *
+ * 2017-10-18 by FDallaire
  */
 
 /* This is an origin request function */
@@ -15,8 +18,9 @@ exports.handler = (event, context, callback) => {
 
     // Keep only the ID initial part together with the search query parameters and hash
     // Example: /4/api-explorer/Coveo-Cloud-V2-APIs?extUrl=1&test=123#somehash  = > /4/?extUrl=1&test=123#somehash
-    request.uri = originalUri.replace(/^(\/[0-9]*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/,"$1$3");
+    request.uri = originalUri.replace(/^(\/[0-9]*\/)([\w\-\.]+[^#?\s]+)$/,"$1");
 
     // quit the lambda and let the request chain continue
     callback( null, request );
 };
+
